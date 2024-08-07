3959ccd8db112afafe1459753f1e9248e1ddb3eb
Tony Le <tonyle@Tonys-MacBook-Air.local>
2024-08-08 04:59:34 +0930

    .

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -1,7 +1,8 @@
 #include "Truckloads.h"
 
 int Truckloads::numTrucks(int numCrates, int loadSize) {
-    if (numCrates < 2 || numCrates > 10000 || loadSize < 1 || loadSize > numCrates - 1) {
+    if (numCrates < 2 || numCrates > 10000 || loadSize < 1) {
+        // || loadSize > (numCrates - 1
         return 0;
     }
     // if (numCrates == 2) {
@@ -14,15 +15,15 @@ int Truckloads::numTrucks(int numCrates, int loadSize) {
     if (numCrates <= loadSize) {
         return 1;
     }
-    // if (numCrates % 2 == 1) {
-    //     int crates = (numCrates-1) / 2;
-    //     return numTrucks(crates + 1, loadSize) + numTrucks(crates, loadSize);
-    // } else {
-    //     int crates = numCrates / 2;
-    //     return numTrucks(crates, loadSize) + numTrucks(crates, loadSize);
-    // }
+    if (numCrates % 2 == 1) {
+        int crates = (numCrates-1) / 2;
+        return numTrucks(crates + 1, loadSize) + numTrucks(crates, loadSize);
+    } else {
+        int crates = numCrates / 2;
+        return numTrucks(crates, loadSize) + numTrucks(crates, loadSize);
+    }
     
-    int halfofCrates = numCrates / 2;
-    int secondHalfofCrates = numCrates - halfofCrates;
-    return numTrucks(halfofCrates, loadSize) + numTrucks(secondHalfofCrates, loadSize);
+    // int halfofCrates = numCrates / 2; //divding floors the value
+    // int secondHalfofCrates = numCrates - halfofCrates; //deals with odds
+    // return numTrucks(halfofCrates, loadSize) + numTrucks(secondHalfofCrates, loadSize);
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
+#include "Truckloads.h"
+#include <iostream>
 
 int main() {
+    Truckloads a;
+    std::cout<<a.numTrucks(4, 2) << std::endl;
+
     return 0;
 }
