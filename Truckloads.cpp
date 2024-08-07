fd8ff7d38f5e1355fde9a769c924a08065b2fb8d
Tony Le <tonyle@Tonys-MacBook-Air.local>
2024-08-08 05:06:44 +0930

    .

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -1,17 +1,18 @@
 #include "Truckloads.h"
 
 int Truckloads::numTrucks(int numCrates, int loadSize) {
-    if (numCrates < 2 || numCrates > 10000 || loadSize < 1) {
-        // || loadSize > (numCrates - 1
+    if (numCrates == 2) {
+        if (loadSize == 2) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+    
+    if (numCrates < 2 || numCrates > 10000 || loadSize < 1 || loadSize > (numCrates - 1)) {
         return 0;
     }
-    // if (numCrates == 2) {
-    //     if (loadSize == 2) {
-    //         return 1;
-    //     } else {
-    //         return 2;
-    //     }
-    // }
+    
     if (numCrates <= loadSize) {
         return 1;
     }
