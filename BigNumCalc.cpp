03ae20a4cc65ea2445c28cbb7321bf236a93792c
Tony Le <tonyle@Tonys-MacBook-Air.local>
2024-09-06 03:54:06 +0930

    .

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -11,10 +11,10 @@ std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2) {
     int carry = 0;
     std::list<int> numlist;
 
-    std::list<int>::iterator it1 = num1.begin();
-    std::list<int>::iterator it2 = num2.begin();
+    std::list<int>::reverse_iterator it1 = num1.rbegin();
+    std::list<int>::reverse_iterator it2 = num2.rbegin();
 
-    for(; it1 != num1.end() && it2 != num2.end(); ++it1, ++it2) {
+    for(; it1 != num1.rend() && it2 != num2.rend(); ++it1, ++it2) {
         int number = *it1 + *it2 + carry;
         numlist.push_front(number%10);
         carry=(number-number%10)/10;
@@ -25,10 +25,10 @@ std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2) {
     int carry = 0;
     std::list<int> numlist;
     
-    std::list<int>::iterator it1 = num1.begin();
-    std::list<int>::iterator it2 = num2.begin();
+    std::list<int>::reverse_iterator it1 = num1.rbegin();
+    std::list<int>::reverse_iterator it2 = num2.rbegin();
 
-    for(; it1 != num1.end() && it2 != num2.end(); ++it1, ++it2) {
+    for(; it1 != num1.rend() && it2 != num2.rend(); ++it1, ++it2) {
         if (*it1 - *it2 - carry < 0) {
             int number = 10 + *it1 - *it2 - carry;
             numlist.push_front(number);
