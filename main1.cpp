cf338921c6b1ccd8bc86b08587a7d518cff35439
Tony Le <tonyle@Tonys-MacBook-Air.local>
2024-09-06 12:46:23 +0930

    .

--- main1.cpp
+++ main1.cpp
@@ -1,12 +1,12 @@
-#include "BigNumCalc.h"
-#include <list>
-#include <string>
+// #include "BigNumCalc.h"
+// #include <list>
+// #include <string>
 
-int main() {
-    BigNumCalc one;
-    std::string numString = "12345";
-    std::list<int> e = one.buildBigNum(numString);
-    std::list<int> d = one.buildBigNum(numString);
-    one.add(e,d);
-    return 0;
-}
+// int main() {
+//     BigNumCalc one;
+//     std::string numString = "12345";
+//     std::list<int> e = one.buildBigNum(numString);
+//     std::list<int> d = one.buildBigNum(numString);
+//     one.add(e,d);
+//     return 0;
+// }
