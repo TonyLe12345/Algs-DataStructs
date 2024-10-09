fa2c90318a38b688c675b9e4aca46cf686973b4b
Tony Le <tonyle@Tonys-MacBook-Air.local>
2024-10-10 00:40:40 +1030

    .

--- klargest.cpp
+++ klargest.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <queue>
 
-int kth_largest(vector<int> values, int k) {
+int kth_largest(std::vector<int> values, int k) {
     std::priority_queue<int> max_priority_queue;
     for (int n : values)
         max_priority_queue.push(n);
